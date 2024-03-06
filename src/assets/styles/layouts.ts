a6226b7b5853d65a6135977c66b38f851ea5c632
ngocDa <ngocda@ows.com.vn>
2024-03-06 15:58:10 +0700

    first commit

---
+++ src/assets/styles/layouts.ts
@@ -0,0 +1,3 @@
+export const padding = {
+  screen: 12,
+};
---
+++ src/assets/styles/typography.ts
@@ -0,0 +1,11 @@
+export const fontSizes = {
+  xxs: 8,
+  xs: 10,
+  sm: 12,
+  md: 14,
+  lg: 16,
+  xl: 18,
+  '2xl': 20,
+  '3xl': 22,
+  '4xl': 24,
+};
